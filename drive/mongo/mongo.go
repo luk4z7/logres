4c0bc8eb9fc412df465acb8858841da525683205
Lucas Alves <luk4z_7@hotmail.com>
2017-08-15 01:27:57 -0300

    Include Package comment on top of files

--- drive/mongo/mongo.go
+++ drive/mongo/mongo.go
@@ -5,7 +5,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// mongo
+// Package mongo provide drive for connections on mongo client and production
 package mongo
 
 import (
--- service/config/config.go
+++ service/config/config.go
@@ -5,7 +5,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// config
+// Package config provide configurations for connections on databases
 package config
 
 import (
